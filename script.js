d5e2f5ccc700efafad124f211e89bda3d4631b51
mann777-ctrl <mjaat2008@gmail.com>
2026-04-11 15:17:03 +0530

    dark_mode_button

--- script.js
+++ script.js
@@ -76,3 +76,20 @@ function applyAll() {
 function goToSignup() {
   window.location.href = "signup.html";
 }
+const toggle = document.getElementById("themeToggle");
+
+// Load saved theme
+if (localStorage.getItem("theme") === "dark") {
+  document.documentElement.setAttribute("data-theme", "dark");
+  toggle.checked = true;
+}
+
+toggle.addEventListener("change", () => {
+  if (toggle.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+  } else {
+    document.documentElement.removeAttribute("data-theme");
+    localStorage.setItem("theme", "light");
+  }
+});
